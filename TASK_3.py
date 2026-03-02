dc49c69bd54728156dd30fbc5ce691e03a37bd4c
anubhavg2024 <anubhav.g2024@gmail.com>
2026-03-02 14:14:27 +0530

    Create TASK_3.py
    This project demonstrates basic web automation testing using Python and Selenium. It includes simple test cases to validate core website functionalities such as login behavior and page responses.

---
+++ TASK_3.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+#Start browser
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+try:
+    print("Opening website...")
+    driver.get("https://www.saucedemo.com/")
+
+    # Test 1: Check if website loads
+    if "saucedemo" in driver.current_url:
+        print("Test 1 Passed: Website loaded successfully")
+    else:
+        print("Test 1 Failed")
+
+    # Test 2: Check for title
+    if driver.title == "Swag Labs":
+        print("Test 2 Passed: Title is correct")
+    else:
+        print("Test 2 Failed")
+
+    # Test 3: Check for logo text
+    logo = driver.find_element(By.CLASS_NAME, "login_logo")
+    if logo.text == "Swag Labs":
+        print("Test 3 Passed: Logo text is correct")
+    else:
+        print("Test 3 Failed")
+
+    time.sleep(3)
+
+finally:
+    driver.quit()
+    print("Browser closed.")
